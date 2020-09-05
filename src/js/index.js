53a902b055f7ef97c5f15cd66821ccec330e52fb
Hazrat Ali <iloveyii@yahoo.com>
2020-09-05 15:06:22 +0200

    Added ES6 class and compile

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,17 @@ const arrow_es6 = name => {
 };
 
 arrow_es6('Ali');
+
+class Student {
+    constructor(name, school) {
+        this.name = name;
+        this.school = school;
+    }
+
+    userInfo() {
+        console.log(`Your name is ${this.name} and you study at ${this.school}`);
+    }
+}
+
+const std = new Student('Kan', 'HKR');
+std.userInfo();
