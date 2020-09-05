2b9fa7676d68cb2326e03397e512babf2f9c296a
Hazrat Ali <iloveyii@yahoo.com>
2020-09-05 14:11:19 +0200

    Webpack fix - add htmlplugin

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',             // Location of main js file
+    output: {                               // Where the bundle file should be saved
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'               // Content path
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',         // Create this file in output.path
+            template: './src/index.html'    // From this template
+        })
+    ]
+};
